ff1be288d38ac7410bb7f5192c98e14e21f03682
gui-massa <99214360+gui-massa@users.noreply.github.com>
2022-09-05 12:49:24 -0300

    Add files via upload

---
+++ mysite/polls/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Question
+
+# Create your views here.
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+def results(request, question_id):
+    question = Question(pk=question_id)
+    context = {'question': question}
+    return render(request, 'polls/results.html', context)
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question, 'error_message': "You didn't select a choice."}
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except KeyError:
+        return render(request, 'polls/vote.html', context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
